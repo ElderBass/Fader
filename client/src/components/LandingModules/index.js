804a832df3fb9e12e4e75ee3dca20ee64ea9f845
Seth Zyg <zygster11@gmail.com>
2021-03-12 12:52:58 -0600

    Signup form successfully linked to the corresponding landing div, and directs user to login page when done.

--- client/src/components/LandingModules/index.js
+++ client/src/components/LandingModules/index.js
@@ -1,36 +1,53 @@
 import React from "react";
-import phonesIcon from '../../assets/phones-icon.png';
-import profileIcon from '../../assets/profile-icon.png';
-import studioIcon from '../../assets/studio-icon.png';
-import './style.css';
+import phonesIcon from "../../assets/phones-icon.png";
+import profileIcon from "../../assets/profile-icon.png";
+import studioIcon from "../../assets/studio-icon.png";
+import { Link } from "react-router-dom";
+import "./style.css";
 
-function LandingModules() {
-    function clickHandler() {
-        console.log("button clicked!");
-    }
+const LandingModules = (props) => {
+  const clickHandler = () => {
+    console.log("button clicked!");
+  };
+
+  //const handleSigup = () => {};
 
   return (
     <div className="containerCustom">
+      <Link to="/signup">
         <div className="gridRow" onClick={clickHandler}>
-            <a href="google.com" className="landingBtn logSignBtn">
-                <img className="landingImg" src={phonesIcon} alt="login/signup modal" onClick={clickHandler} />
-            </a>
-            <p className="btnText">Log in or signup here!</p>
+          <img
+            className="landingImg"
+            src={phonesIcon}
+            alt="login/signup modal"
+          />
+          <p className="btnText">Log in or signup here!</p>
         </div>
-        <div className="gridRow">
-            <a href="google.com" className="landingBtn" id="browseBtn">
-                <img className="landingImg" src={profileIcon} alt="browse artists modal" onClick={clickHandler} />
-            </a>
-            <p className="btnText">Browse artists here!</p>
-        </div>
-        <div className="gridRow">
-            <a href="google.com" className="landingBtn" id="placeholderBtn">
-                <img className="landingImg" src={studioIcon} alt="PLACEHOLDER modal" onClick={clickHandler} />
-            </a>
-            <p className="btnText">PLACEHOLDER</p>
-        </div>
+      </Link>
+      <div className="gridRow">
+        <a href="google.com" className="landingBtn" id="browseBtn">
+          <img
+            className="landingImg"
+            src={profileIcon}
+            alt="browse artists modal"
+            onClick={clickHandler}
+          />
+        </a>
+        <p className="btnText">Browse artists here!</p>
+      </div>
+      <div className="gridRow">
+        <a href="google.com" className="landingBtn" id="placeholderBtn">
+          <img
+            className="landingImg"
+            src={studioIcon}
+            alt="PLACEHOLDER modal"
+            onClick={clickHandler}
+          />
+        </a>
+        <p className="btnText">PLACEHOLDER</p>
+      </div>
     </div>
   );
-}
+};
 
 export default LandingModules;
