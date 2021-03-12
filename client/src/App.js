79e147a14115cd9788dfaa26614872f039cf79fa
Carson Scholberg <cscholbe@gustavus.edu>
2021-03-12 11:40:33 -0600

    landing modules w/ hover and depress actions wow!

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,20 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
-import SignupForm from "./components/SignupForm";
+// import SignupForm from "./components/SignupForm";
 import LoginForm from "./components/LoginForm";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Browse from "./pages/BrowseArtists/BrowseArtists";
 import "./App.css";
+import LandingModules from "./components/LandingModules";
 
 function App() {
   return (
     <>
       <Router>
         <Navbar />
-        <SignupForm />
+        {/* <SignupForm /> */}
         <LoginForm />
         <div className="mainContainer">
           <Route exact path="/" component={Home} />
@@ -21,6 +22,7 @@ function App() {
           {/* <Route exact path="/drumpad" component={Drumpad} />
         <Route exact path="/profile" component={UserProfile} />
         <Route path="/artists" component={AllArtists} /> */}
+        <LandingModules />
         </div>
         <Footer/>
       </Router>
