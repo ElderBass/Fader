286797d2a79fd815f96f34dbef18f2c899648250
Carson Scholberg <cscholbe@gustavus.edu>
2021-03-17 10:24:31 -0500

    signup and login messages css fix

--- client/src/components/Signup/SignupMessage/index.js
+++ client/src/components/Signup/SignupMessage/index.js
@@ -12,14 +12,14 @@ return (
       <div className="text-center">
         <h4 className="signupMessageHeader">Fade In</h4>
       </div>
-      <hr />
+      <br />
       <div className="text-center">
-        <h6>Rock on. Welcome to Fader. Now just sign in.</h6>
+        <h6 className="signupMessage">Rock on. Welcome to Fader. Now just sign in.</h6>
       </div>
-      <hr />
+      <br />
       <div className="text-center">
         <Link to="/login">
-        <button type="submit" className="btn btn-secondary" id="buttonText" >Go to Login</button>
+        <button type="submit" className="btn btn-secondary" id="signupMessageBtn" >Go to Login</button>
         </Link>
       </div>{" "}
     </div>
